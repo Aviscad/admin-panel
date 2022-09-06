1601002646fe578cff63acaf51bd0b13e827738b
Aviscad <anthonyviscad@gmail.com>
2022-09-06 17:24:07 -0600

    Finishing Responsive View

--- js/script.js
+++ js/script.js
@@ -1,30 +1,20 @@
-let dashboard = document.getElementsByClassName("dashboard"),
+let dashboard = document.getElementsByClassName("dashboard-title"),
   dashboardGroup = document.getElementsByClassName("dashboard-group");
 
 window.addEventListener("load", () => {
-  if (window.screen.width > 767) {
+  if (window.innerWidth >= 768) {
     for (let i = 0; i < dashboardGroup.length; i++) {
       dashboardGroup[i].classList.remove("hidden");
       dashboardGroup[i].classList.remove("visuallyHidden");
     }
-  }
-});
-window.addEventListener("resize", () => {
-  if (window.screen.width > 767) {
-    for (let i = 0; i < dashboardGroup.length; i++) {
-      dashboardGroup[i].classList.remove("hidden");
-      dashboardGroup[i].classList.remove("visuallyHidden");
-    }
-  }
-});
-
-dashboard[0].addEventListener("click", (e) => {
-  if (window.screen.width < 768) {
-    for (let i = 0; i < dashboardGroup.length; i++) {
-      dashboardGroup[i].classList.toggle("hidden");
-      setTimeout(() => {
-        dashboardGroup[i].classList.toggle("visuallyHidden");
-      }, 100);
-    }
+  } else {
+    dashboard[0].addEventListener("click", (e) => {
+      for (let i = 0; i < dashboardGroup.length; i++) {
+        dashboardGroup[i].classList.toggle("hidden");
+        setTimeout(() => {
+          dashboardGroup[i].classList.toggle("visuallyHidden");
+        }, 100);
+      }
+    });
   }
 });
